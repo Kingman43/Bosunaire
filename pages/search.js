44625a97a69e9ec306956db5a4e8cae8c87838b3
Kingman43 <mark_king@yahoo.com>
2023-03-08 17:34:32 -0500

    First cut at the map

--- pages/search.js
+++ pages/search.js
@@ -1,12 +1,24 @@
 import BoHome from "@/components/bohome.js";
-
+import Map from '@/components/map';
 export default function Search() {
-
+    const position = [35.91086, -78.69078];
+    const zoom = 15;
+    const marker = [35.91086, -78.69078];
     return(
-        <div className="flex flex-col py-1" >
-            <h1 className="py-8">Show the map with search bar, etc</h1>
+        <div className="" >
+            <h1 className="py-8">Trying to get the map to work</h1>
 
-            <BoHome/>
+            <div>
+                <Map
+                 position={position}
+                 zoom={zoom}
+                 marker={marker}
+                />
+            </div>
+
+            <div className="pt-3">
+                <BoHome/>
+            </div>
         </div>
 
 
