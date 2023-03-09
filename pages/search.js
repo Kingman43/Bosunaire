d214b3d22f1016ebdc7c1c8d2227ac19a5245a93
Kingman43 <44009057+Kingman43@users.noreply.github.com>
2023-03-08 21:41:43 -0500

    Multiple map markers

--- pages/search.js
+++ pages/search.js
@@ -1,18 +1,25 @@
 import BoHome from "@/components/bohome.js";
 import Map from '@/components/map';
 export default function Search() {
+    const locations = [
+        {"name": "Mark King was here", position: [35.91086, -78.69078]},
+        {"name": "Beth Allen was here", position: [35.7, -78.3]},
+    ];
     const position = [35.91086, -78.69078];
-    const zoom = 10;
+    const zoom = 9;
     const marker = [35.91086, -78.69078];
+    const popupText = "Mark King was here"
     return(
         <div className="" >
-            <h1 className="py-8">Trying to get the map to work</h1>
+            <h1 className="p-3">Testing out some map stuff</h1>
 
             <div>
                 <Map
-                 position={position}
+                    position={position}
+                 locations={locations}
                  zoom={zoom}
                  marker={marker}
+                 mypopup={popupText}
                 />
             </div>
 
