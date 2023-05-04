f4a16c79753e95881f1f99d15af1996c6c6d94b7
Kingman43 <44009057+Kingman43@users.noreply.github.com>
2023-05-04 13:46:36 -0400

    Made map filter permanent. And made mapFilter it's on component

--- pages/search.js
+++ pages/search.js
@@ -1,10 +1,10 @@
 import BoHome from "@/components/bohome.js";
 import MyButton from "@/components/mybutton.js";
-import MySelect from "@/components/myselect.js";
 import Map from '@/components/map';
 import {useState} from "react";
 import {mapContext} from "@/components/Context";
 import { round } from 'lodash';
+import MapFilter from "@/components/mapFilter";
 
 let numHosts = 1;
 //let origPosition = [ 37.0902, -95.7129];
@@ -19,16 +19,9 @@ export default function Search() {
     const [myText, setMyText] = useState(numHosts + " Bosunaire hosts with 10 miles");
     const [position, setPosition] = useState( origPosition);
     const [markerPosition, setMarkerPosition] = useState( origPosition);
-    const [listingType, setType] = useState( 0);
     const [bShowSearchButton, setShowButton] = useState( false);
-    const [showModal, setShowModal] = useState(false);
 
-    const listingTypes = [
-        { label: 'All Listings', value: 0 },
-        { label: 'Homes for Sale', value: 1 },
-        { label: 'Bed and Breakfasts', value: 2 },
-        { label: 'Event Hosts', value: 3 },
-    ];
+    const parms = {};
 
     function reSubmit() {
         console.log('You clicked me! NewPostion:', position);
@@ -36,9 +29,11 @@ export default function Search() {
         getNewValues();
     }
     function searchPerformed (location) {
+
         getNewValues();
         setMarkerPosition({ lat: location.y, lng: location.x });
         setPosition({ lat: location.y, lng: location.x });
+        setShowButton(false);
         console.log("searchPerformed:" , [location.y, location.x]);
     }
     function getNewValues () {
@@ -46,67 +41,52 @@ export default function Search() {
         setShowButton(false);
         setMyText(numHosts + " Bosunaire Hosts within 10 miles");
     }
-    const newListingType = (event) => {
-        setType(event.target.value);
-        setShowButton(true)
-        setShowModal(false);
-    };
-    function filterModal () {
-        setShowModal(true);
-    }
-    function FilterButton () {
-            return <MyButton func={filterModal} text='Filter'/>
-    }
 
     function SubmitButton () {
-        if ( (round(position.lat, 4) !== round(markerPosition.lat,4) ) ||
-             (round(position.lng,4) !== round(markerPosition.lng,4) ) ||
-                bShowSearchButton) {
-                console.log("pos.lat:", round(position.lat,3));
-                console.log("mar.lat:", round(markerPosition.lat,3));
-                console.log("pos.lng:", round(position.lng,3));
-                console.log("mar.lng:", round(markerPosition.lng,3));
-                return <MyButton func={reSubmit} text='Search Again'/>
+        if ( (round(position.lat, 3) !== round(markerPosition.lat,3) ) ||
+            (round(position.lng,3) !== round(markerPosition.lng,3) ) ||
+            bShowSearchButton) {
+            console.log("pos.lat:", round(position.lat,3));
+            console.log("mar.lat:", round(markerPosition.lat,3));
+            console.log("pos.lng:", round(position.lng,3));
+            console.log("mar.lng:", round(markerPosition.lng,3));
+            console.log("bShowSearchButton:", bShowSearchButton);
+            return <MyButton func={reSubmit} text='Search Again' bActive={true}/>
         } else {
-            return null;
+            return <MyButton func={reSubmit} text='Search Again' bActive={false}/>
             // return <MyButton text='Search is up-to-date'/>
         }
     }
+    function FilterChanged() {
+        setShowButton(true);
+    }
+    function FilterCancel() {
 
+    }
     return(
         <mapContext.Provider value={{position, setPosition, markerPosition, setMarkerPosition}}>
-        <div className="mx-auto max-w-prose " >
-            Testing out some map stuff
-            <div>
-                <Map
-                    text={myText}
-                    textWords={myText}
-                    markerPosition={markerPosition}
-                    searchPerformed={searchPerformed}
-                />
+            <div className="mx-auto max-w-prose " >
+                Testing out some map stuff
+                <div>
+                    <Map
+                        text={myText}
+                        textWords={myText}
+                        markerPosition={markerPosition}
+                        searchPerformed={searchPerformed}
+                    />
+                </div>
+
+                <div className="mx-auto max-w-prose flex justify-around items-center">
+                    <SubmitButton/>
+                </div>
+
+                <MapFilter onFilterChange={FilterChanged} onCancel={FilterCancel} parms={parms}/>
+
+
+                <div className="pt-3">
+                    <BoHome/>
+                </div>
             </div>
-
-            <div className="mx-auto max-w-prose flex justify-around items-center">
-                <SubmitButton/>
-                <FilterButton/>
-            </div>
-
-                {showModal ?
-                    <div>
-                        <MySelect
-                            options={listingTypes}
-                            value={listingType}
-                            onChange={newListingType}
-                        />
-                    </div>
-                    : null
-                }
-
-
-            <div className="pt-3">
-                <BoHome/>
-            </div>
-        </div>
         </mapContext.Provider>
     )
 }
